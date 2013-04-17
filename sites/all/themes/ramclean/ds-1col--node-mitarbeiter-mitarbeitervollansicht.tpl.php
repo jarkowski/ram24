240b1eaba3bc4687bdc2f240910706f295e088a8
djar <djar@gmx.de>
2013-04-17 15:19:36 +0200

    theming ma page ok

--- sites/all/themes/ramclean/ds-1col--node-mitarbeiter-mitarbeitervollansicht.tpl.php
+++ sites/all/themes/ramclean/ds-1col--node-mitarbeiter-mitarbeitervollansicht.tpl.php
@@ -4,15 +4,32 @@
  * @file
  * Display Suite 1 column template.
  */
+
+dpm($variables);
 ?>
 <<?php print $ds_content_wrapper; print $layout_attributes; ?> class="ds-1col <?php print $classes;?> clearfix">
 
-  <?php if (isset($title_suffix['contextual_links'])): ?>
-  <?php print render($title_suffix['contextual_links']); ?>
-  <?php endif; ?>
 
-  <?php print $ds_content; ?>
+
+
+<div id="ma_bild_wrapper">
+ <?php print render($content['field_mitarbeiter_bild']); ?>
+</div>
+
+<div id="right_content_wrapper">
+<?php print render($content['field_mitarbeiter_vorname']); ?>
+<?php print render($content['field_mitarbeiter_nachname']); ?>
+<?php print render($content['field_position']); ?>
+<?php print render($content['body']); ?>
+<?php print render($content['field_mitarbeiter_spezialgebiete']); ?>
+<?php print render($content['field_mitarbeiter_telefon']); ?>
+<?php print render($content['field_mitarbeiter_email']); ?>
+</div>
+
 </<?php print $ds_content_wrapper ?>>
+
+
+
 
 <?php if (!empty($drupal_render_children)): ?>
   <?php print $drupal_render_children ?>
